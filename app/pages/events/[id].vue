2875a0de6e475b320b32a38263480754615c6e06
leoVolk <leovolkmann@googlemail.com>
2026-02-17 17:51:09 +0100

    feat: :sparkles: updated event page

--- app/pages/events/[id].vue
+++ app/pages/events/[id].vue
@@ -17,56 +17,82 @@
         </div>
       </template>
       <template #default>
-        <!-- TODO: rework design -->
         <div class="flex gap-8 flex-col lg:flex-row">
-          <UPageCard
-            icon="i-lucide-clipboard-list"
-            title="Material Listen"
-            class="w-full lg:max-w-1/3"
-          >
-            <template #description>
-              <div class="gap-4 py-2 flex-col flex">
-                <ULink
-                  v-for="value in lists"
-                  :to="`lists/${value.id}`"
-                  class="flex gap-2 items-center underline"
-                >
-                  <UIcon name="i-lucide-list" class="size-4"></UIcon>
-                  <span>{{ value.name }}</span>
-                </ULink>
+          <div class="w-full max-w-1/3">
+            <div class="flex justify-between items-center">
+              <div class="flex-col flex gap-2">
+                <UIcon
+                  name="i-lucide-clipboard-list"
+                  class="size-6 text-primary"
+                ></UIcon>
+                <h4 class="text-xl font-semibold">Material Listen</h4>
+              </div>
 
-                <CreateEventList
-                  @refresh="getEventInfos()"
-                  :event-id="event.id"
-                ></CreateEventList>
+              <div>
+                <UButton icon="i-lucide-plus"></UButton>
               </div>
-            </template>
-          </UPageCard>
-          <UPageCard
-            icon="i-lucide-notebook-pen"
-            title="Notizen"
-            class="w-full lg:max-w-1/3"
-          >
-            <template #description>
-              <div class="gap-4 py-2 flex-col flex">
-                <ULink
-                  v-for="value in notes"
-                  :to="`notes/${value.id}`"
-                  class="flex gap-2 items-center underline"
-                >
-                  <UIcon name="i-lucide-notebook" class="size-4"></UIcon>
-                  <span>{{ value.name }}</span>
-                </ULink>
+            </div>
+
+            <div class="pt-4">
+              <ULink
+                v-for="value in lists"
+                :to="`lists/${value.id}`"
+                class="flex gap-2 items-center underline"
+              >
+                <UIcon name="i-lucide-list" class="size-4"></UIcon>
+                <span>{{ value.name }}</span>
+              </ULink>
+            </div>
+          </div>
+          <div class="w-full max-w-1/3">
+            <div class="flex justify-between items-center">
+              <div class="flex-col flex gap-2">
+                <UIcon
+                  name="i-lucide-notebook-pen"
+                  class="size-6 text-primary"
+                ></UIcon>
+                <h4 class="text-xl font-semibold">Notizen</h4>
               </div>
-            </template>
-          </UPageCard>
-          <UPageCard
-            icon="i-lucide-users"
-            title="Teilnehmer Listen"
-            class="w-full lg:max-w-1/3"
-          >
-            <template #description> </template>
-          </UPageCard>
+
+              <div>
+                <UButton icon="i-lucide-plus"></UButton>
+              </div>
+            </div>
+
+            <div class="pt-4">
+              <ULink
+                v-for="value in notes"
+                :to="`lists/${value.id}`"
+                class="flex gap-2 items-center underline"
+              >
+                <UIcon name="i-lucide-notebook" class="size-4"></UIcon>
+                <span>{{ value.name }}</span>
+              </ULink>
+            </div>
+          </div>
+          <div class="w-full max-w-1/3">
+            <div class="flex justify-between items-center">
+              <div class="flex-col flex gap-2">
+                <UIcon name="i-lucide-list" class="size-6 text-primary"></UIcon>
+                <h4 class="text-xl font-semibold">Teilnehmer Listen</h4>
+              </div>
+
+              <div>
+                <UButton icon="i-lucide-plus"></UButton>
+              </div>
+            </div>
+
+            <div class="pt-4">
+              <ULink
+                v-for="value in participantLists"
+                :to="`lists/${value.id}`"
+                class="flex gap-2 items-center underline"
+              >
+                <UIcon name="i-lucide-list" class="size-4"></UIcon>
+                <span>{{ value.name }}</span>
+              </ULink>
+            </div>
+          </div>
         </div>
       </template>
     </UCard>
---
+++ app/pages/events/notes/create.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="flex flex-col gap-4">
+    <div class="flex justify-between items-center">
+      <UBreadcrumb
+        :items="[
+          { label: 'Home', to: '/' },
+          { label: 'Läger & Aktionen', to: '/events' },
+          { label: 'Notizen', to: '/events' },
+          { label: note.name },
+        ]"
+      />
+    </div>
+
+    <UCard>
+      <template #header
+        ><div class="flex justify-between items-center">
+          <h2 class="text-2xl">{{ note.name }}</h2>
+
+          <UButton
+            label="Bearbeiten"
+            icon="i-lucide-edit"
+            @click="isEditing = !isEditing"
+          ></UButton>
+        </div>
+      </template>
+
+      <template #default>
+        <div v-if="isEditing">
+          <UEditor
+            v-slot="{ editor }"
+            v-model="note.content"
+            :handlers="customHandlers"
+            content-type="html"
+            :ui="{ base: 'p-8 sm:px-16' }"
+            class="w-full min-h-74"
+          >
+            <UEditorToolbar
+              :editor="editor"
+              :items="items"
+              class="border-b border-muted py-2 overflow-x-auto"
+            />
+          </UEditor>
+
+          <div class="flex justify-end">
+            <UButton
+              label="Speichern"
+              icon="i-lucide-save"
+              @click="onSubmit"
+            ></UButton>
+          </div>
+        </div>
+        <MDC v-else :value="note.content" tag="article" />
+      </template>
+    </UCard>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { EditorCustomHandlers, EditorToolbarItem } from "@nuxt/ui";
+import type { Editor } from "@tiptap/vue-3";
+
+definePageMeta({
+  middleware: ["auth"],
+});
+
+const { pb } = usePocketbase();
+const route = useRoute();
+
+const toast = useToast();
+const loading = ref(false);
+
+const note = ref();
+const isEditing = ref(false);
+
+const state = reactive({
+  name: "",
+  content: "",
+});
+
+const customHandlers = {
+  imageUpload: {
+    canExecute: (editor: Editor) =>
+      editor.can().insertContent({ type: "imageUpload" }),
+    execute: (editor: Editor) =>
+      editor.chain().focus().insertContent({ type: "imageUpload" }),
+    isActive: (editor: Editor) => editor.isActive("imageUpload"),
+    isDisabled: undefined,
+  },
+} satisfies EditorCustomHandlers;
+
+const items = [
+  [
+    {
+      icon: "i-lucide-heading",
+      content: {
+        align: "start",
+      },
+      items: [
+        {
+          kind: "heading",
+          level: 1,
+          icon: "i-lucide-heading-1",
+          label: "Überschrift 1",
+        },
+        {
+          kind: "heading",
+          level: 2,
+          icon: "i-lucide-heading-2",
+          label: "Überschrift 2",
+        },
+        {
+          kind: "heading",
+          level: 3,
+          icon: "i-lucide-heading-3",
+          label: "Überschrift 3",
+        },
+        {
+          kind: "heading",
+          level: 4,
+          icon: "i-lucide-heading-4",
+          label: "Überschrift 4",
+        },
+      ],
+    },
+  ],
+  [
+    {
+      kind: "mark",
+      mark: "bold",
+      icon: "i-lucide-bold",
+    },
+    {
+      kind: "mark",
+      mark: "italic",
+      icon: "i-lucide-italic",
+    },
+    {
+      kind: "mark",
+      mark: "underline",
+      icon: "i-lucide-underline",
+    },
+    {
+      kind: "mark",
+      mark: "strike",
+      icon: "i-lucide-strikethrough",
+    },
+    {
+      kind: "mark",
+      mark: "code",
+      icon: "i-lucide-code",
+    },
+  ],
+] satisfies EditorToolbarItem<typeof customHandlers>[][];
+
+const onSubmit = async () => {
+  loading.value = true;
+
+  await pb.collection("eventnotes").create(state);
+
+  toast.add({
+    title: "Notiz aktualisiert",
+    icon: "i-lucide-save",
+  });
+
+  loading.value = false;
+  isEditing.value = false;
+};
+</script>
+
+<style></style>
