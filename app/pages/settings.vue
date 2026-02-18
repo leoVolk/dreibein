80c77daccf97fd4ac505233168456c16f292d443
leoVolk <leovolkmann@googlemail.com>
2026-02-18 18:52:09 +0100

    feat: :sparkles: updated settings page to allow import

--- app/pages/settings.vue
+++ app/pages/settings.vue
@@ -9,7 +9,7 @@
 
     <div class="flex flex-col gap-8">
       <div class="pt-4">
-        <UCard>
+        <UCard variant="subtle">
           <template #header>
             <h3 class="text-xl flex items-center gap-2">
               <UIcon name="i-lucide-user" class="size-6" /> Benutzer
@@ -20,6 +20,7 @@
             <UForm class="flex flex-col gap-4" :state="userState">
               <UFormField label="Benutzername" class="w-1/2">
                 <UInput
+                  variant="subtle"
                   v-model="userState.username"
                   size="lg"
                   class="w-full"
@@ -38,27 +39,85 @@
       </div>
 
       <div v-if="user?.admin">
-        <UCard>
+        <UCard variant="subtle">
           <template #header>
             <div class="flex justify-between items-center">
               <h3 class="text-xl flex items-center gap-2">
                 <UIcon name="i-lucide-shield-user" class="size-6" /> Admin
                 Einstellungen
               </h3>
-
-              <CreateUser @refresh="getUsers()"></CreateUser>
             </div>
           </template>
 
           <template #default>
-            <UTable :data="users" :columns="columns">
-              <template #admin-cell="{ row }">
-                <UCheckbox
-                  :disabled="true"
-                  v-model="row.original.admin"
-                ></UCheckbox>
-              </template>
-            </UTable>
+            <div class="flex flex-col gap-4">
+              <div>
+                <div class="flex justify-between items-center mb-4">
+                  <h4 class="text-lg flex items-center gap-2">
+                    <UIcon name="i-lucide-users" class="size-6" />
+                    <span>Benutzer</span>
+                  </h4>
+
+                  <CreateUser @refresh="getUsers()"></CreateUser>
+                </div>
+                <UTable :data="users" :columns="userColumns">
+                  <template #admin-cell="{ row }">
+                    <UCheckbox
+                      :disabled="true"
+                      v-model="row.original.admin"
+                    ></UCheckbox>
+                  </template>
+                </UTable>
+              </div>
+
+              <USeparator class="h-4"></USeparator>
+
+              <div class="flex flex-col gap-4">
+                <div class="flex justify-between items-center">
+                  <h4 class="text-lg flex items-center gap-2">
+                    <UIcon name="i-lucide-list-plus" class="size-6" />
+                    <span>NaMi Mitglieder Import</span>
+                  </h4>
+                </div>
+
+                <UAlert
+                  color="warning"
+                  title="Bei Import werden alle vorherigen Mitglieder & evtl. vorgenommenen Änderungen überschrieben bzw gelöscht."
+                  icon="i-lucide-triangle-alert"
+                />
+
+                <div class="flex items-center gap-4">
+                  <p class="text-lg">
+                    {{ namiFile ? namiFile.name : "Datei auswählen" }}
+                  </p>
+                  <UFileUpload
+                    class="max-w-full"
+                    size="xl"
+                    variant="button"
+                    label="Excel Datei hier ablegen"
+                    accept=".xlsx, .xls"
+                    v-model="namiFile"
+                    @update:model-value="handleFileUpload"
+                  />
+                </div>
+
+                <UTable
+                  class="max-h-96"
+                  sticky
+                  v-if="namiFileData.length"
+                  :columns="namiColumns"
+                  :data="namiFileData"
+                ></UTable>
+
+                <div v-if="namiFileData.length" class="flex justify-end">
+                  <UButton
+                    icon="i-lucide-import"
+                    @click="onUserNameChange()"
+                    label="Importieren"
+                  ></UButton>
+                </div>
+              </div>
+            </div>
           </template>
         </UCard>
       </div>
@@ -67,6 +126,7 @@
 </template>
 <script setup lang="ts">
 import type { TableColumn } from "@nuxt/ui";
+import * as XLSX from "xlsx";
 
 const { user } = usePocketbaseAuth();
 const { pb } = usePocketbase();
@@ -95,7 +155,6 @@ const onUserNameChange = async () => {
 };
 
 const users = ref();
-const currentUser = ref();
 
 const getUsers = async () => {
   if (!user.value?.admin) return;
@@ -108,7 +167,7 @@ onMounted(async () => {
   userState.username = user.value?.name;
 });
 
-const columns: TableColumn<any>[] = [
+const userColumns: TableColumn<any>[] = [
   { header: "Name", accessorKey: "name" },
   {
     header: "E-Mail",
@@ -126,4 +185,38 @@ const columns: TableColumn<any>[] = [
     cell: ({ row }) => new Date(row.getValue("updated")).toLocaleDateString(),
   },
 ];
+const namiFile = ref();
+const namiFileData = ref<any[]>([]);
+
+const handleFileUpload = (file: any) => {
+  if (!file) return;
+
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const data = new Uint8Array(e.target?.result as ArrayBuffer);
+    const workbook = XLSX.read(data, { type: "array" });
+
+    const sheetName = workbook.SheetNames[0] as string;
+    const worksheet = workbook.Sheets[sheetName];
+
+    namiFileData.value = XLSX.utils.sheet_to_json(worksheet as XLSX.WorkSheet);
+    createTableHeaders();
+  };
+  reader.readAsArrayBuffer(file);
+};
+
+const namiColumns = ref<TableColumn<any>[]>();
+
+const createTableHeaders = () => {
+  const columns: TableColumn<any>[] = Object.keys(namiFileData.value[0]).map(
+    (col) => {
+      return {
+        header: col,
+        accessorKey: col,
+      };
+    },
+  );
+
+  namiColumns.value = columns;
+};
 </script>
