6c74db0f90d7b0ac4f033726a636f3f705a54432
leoVolk <leovolkmann@googlemail.com>
2026-03-10 12:21:29 +0100

    feat: :sparkles: updated colours

--- app/components/Event/AddEventItem.vue
+++ app/components/Event/AddEventItem.vue
@@ -1,6 +1,6 @@
 <template>
   <UDrawer :open="open" direction="right" :handle="false" :dismissible="false">
-    <UButton color="success" icon="i-lucide-plus" @click="open = true"
+    <UButton color="primary" icon="i-lucide-plus" @click="open = true"
       >Material hinzufügen</UButton
     >
 
