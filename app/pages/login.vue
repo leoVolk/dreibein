886e32dfe69a4b953d49edfc23fb58592380d06d
leoVolk <leovolkmann@googlemail.com>
2026-03-11 18:05:36 +0100

    feat: :sparkles: updated stylings

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -7,7 +7,7 @@
     <UPageCard class="w-full max-w-md">
       <UIcon name="i-lucide-user" class="size-12 mx-auto mb-4"></UIcon>
       <h1 class="text-2xl text-center font-bold mb-4">
-        Bei <span class="underline">Dreibein</span> anmelden
+        Bei <span class="underline">3Bein</span> anmelden
       </h1>
       <div class="flex flex-col gap-4">
         <UFormField label="Email" required>
